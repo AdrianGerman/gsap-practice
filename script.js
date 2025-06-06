1d261b41ed3ac0b074b15224fd85cf3a544a50fb
AdrianGerman <adrian.german1019@gmail.com>
2025-06-06 07:49:36 -0600

    new options for animations

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-gsap.to(".box", { x: 100, duration: 1 })
+gsap.to(".box", {
+  x: 100,
+  duration: 1,
+  rotate: 360,
+  scale: 0.5,
+  stagger: {
+    each: 0.2,
+    repeat: -1,
+    yoyo: true
+  }
+})
